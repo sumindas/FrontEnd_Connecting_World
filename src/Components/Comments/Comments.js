e4cfc85e813569313961972c0c34bf673b4c6fb0
sumindas <vrsumindas007@gmail.com>
2024-03-09 01:43:13 +0530

    coment media2

--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -4,13 +4,13 @@ import { BASE_URL } from "../../Api/api";
 import { useSelector } from "react-redux";
 
 function CommentsList({ post }) {
- const [comments, setComments] = useState([]);
- const currentUser = useSelector((state) => state.auth.user);
- const token = localStorage.getItem("token");
- const postId = post.id;
- const userId = localStorage.getItem("userId");
+  const [comments, setComments] = useState([]);
+  const currentUser = useSelector((state) => state.auth.user);
+  const token = localStorage.getItem("token");
+  const postId = post.id;
+  const userId = localStorage.getItem("userId");
 
- useEffect(() => {
+  useEffect(() => {
     const fetchComments = async () => {
       try {
         const response = await axios.get(
@@ -31,9 +31,9 @@ function CommentsList({ post }) {
     };
 
     fetchComments();
- }, [post.id, userId]);
+  }, [post.id, userId]);
 
- const handleCommentSubmit = async (e) => {
+  const handleCommentSubmit = async (e) => {
     e.preventDefault();
     const commentText = e.target.comment.value;
 
@@ -53,10 +53,10 @@ function CommentsList({ post }) {
     } catch (error) {
       console.error("Error posting comment:", error);
     }
- };
+  };
 
-// sourcery skip: avoid-function-declarations-in-blocks
- function ReplyForm({ commentId, userId, onSubmit }) {
+  // sourcery skip: avoid-function-declarations-in-blocks
+  function ReplyForm({ commentId, userId, onSubmit }) {
     const handleSubmit = async (e) => {
       e.preventDefault();
       const commentText = e.target.comment.value;
@@ -94,91 +94,93 @@ function CommentsList({ post }) {
         </button>
       </form>
     );
- }
+  }
 
- return (
-  <div className="w-full bg-white p-4 rounded-lg flex flex-col gap-4 m-4">
-     <div className="writebox">
-       <form onSubmit={handleCommentSubmit} className="flex flex-col sm:flex-row">
-         <div className="flex items-center">
-           {currentUser && currentUser.user_profile ? (
-             <img
-               src={`${BASE_URL}${currentUser.user_profile.profile_image}`}
-               alt=""
-               className="rounded-full w-8 h-8 mr-2"
-             />
-           ) : null}
-           <input
-             type="text"
-             name="comment"
-             placeholder="Write a comment"
-             className="flex-grow py-1 px-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-           />
-           <button
-             type="submit"
-             className="ml-2 py-1 px-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-           >
-             Comment
-           </button>
-         </div>
-       </form>
-     </div>
-     {comments.length === 0 ? (
-       <p className="text-center">No comments yet.</p>
-     ) : (
-       comments.map((comment) => (
-         <div key={comment.id} className="flex flex-col mb-4">
-           <div className="flex items-center">
-             {comment.user && comment.user.userprofile ? (
-               <img
-                 src={`${comment.user.userprofile.profile_image}`}
-                 alt=""
-                 className="rounded-full w-8 h-8 mr-2"
-               />
-             ) : null}
-             <div>
-               <h5 className="font-semibold text-sm">
-                 {comment.user ? comment.user.username : "Anonymous"}
-               </h5>
-               <p className="text-sm">{comment.content}</p>
-             </div>
-           </div>
-           {comment.replies?.map((reply) => (
-             <div key={reply.id} className="flex items-center ml-4 mt-1">
-               {reply.user && reply.user.userprofile ? (
-                 <img
-                  src={`${BASE_URL}${reply.user.userprofile.profile_image}`}
+  return (
+    <div className="w-full bg-white p-4 rounded-lg flex flex-col gap-4 m-4">
+      <div className="writebox">
+        <form
+          onSubmit={handleCommentSubmit}
+          className="flex flex-col sm:flex-row"
+        >
+          <div className="flex items-center">
+            {currentUser && currentUser.user_profile ? (
+              <img
+                src={`${BASE_URL}${currentUser.user_profile.profile_image}`}
+                alt=""
+                className="rounded-full w-8 h-8 mr-2"
+              />
+            ) : null}
+            <input
+              type="text"
+              name="comment"
+              placeholder="Write a comment"
+              className="flex-grow py-1 px-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent sm:text-sm"
+            />
+            <button
+              type="submit"
+              className="ml-2 py-1 px-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 sm:text-sm sm:py-0.5 sm:px-1 sm:text-xs"
+            >
+              Comment
+            </button>
+          </div>
+        </form>
+      </div>
+      {comments.length === 0 ? (
+        <p className="text-center">No comments yet.</p>
+      ) : (
+        comments.map((comment) => (
+          <div key={comment.id} className="flex flex-col mb-4">
+            <div className="flex items-center">
+              {comment.user && comment.user.userprofile ? (
+                <img
+                  src={`${comment.user.userprofile.profile_image}`}
                   alt=""
                   className="rounded-full w-8 h-8 mr-2"
-                 />
-               ) : null}
-               <div>
-                 <h5 className="font-semibold text-sm">
-                  {reply.user ? reply.user.username : "Anonymous"}
-                 </h5>
-                 <p className="text-sm">{reply.content}</p>
-               </div>
-             </div>
-           ))}
-           <ReplyForm
-             commentId={comment.id}
-             userId={userId}
-             onSubmit={(newReply) => {
-               setComments(
-                 comments.map((c) =>
-                  c.id === comment.id
-                     ? { ...c, replies: [...c.replies, newReply] }
-                     : c
-                 )
-               );
-             }}
-           />
-         </div>
-       ))
-     )}
-  </div>
- );
- 
+                />
+              ) : null}
+              <div>
+                <h5 className="font-semibold text-sm sm:text-xs">
+                  {comment.user ? comment.user.username : "Anonymous"}
+                </h5>
+                <p className="text-sm sm:text-xs">{comment.content}</p>
+              </div>
+            </div>
+            {comment.replies?.map((reply) => (
+              <div key={reply.id} className="flex items-center ml-4 mt-1">
+                {reply.user && reply.user.userprofile ? (
+                  <img
+                    src={`${BASE_URL}${reply.user.userprofile.profile_image}`}
+                    alt=""
+                    className="rounded-full w-8 h-8 mr-2"
+                  />
+                ) : null}
+                <div>
+                  <h5 className="font-semibold text-sm sm:text-xs">
+                    {reply.user ? reply.user.username : "Anonymous"}
+                  </h5>
+                  <p className="text-sm sm:text-xs">{reply.content}</p>
+                </div>
+              </div>
+            ))}
+            <ReplyForm
+              commentId={comment.id}
+              userId={userId}
+              onSubmit={(newReply) => {
+                setComments(
+                  comments.map((c) =>
+                    c.id === comment.id
+                      ? { ...c, replies: [...c.replies, newReply] }
+                      : c
+                  )
+                );
+              }}
+            />
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default CommentsList;
